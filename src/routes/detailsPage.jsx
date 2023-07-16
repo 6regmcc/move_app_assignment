a991d3f42e2b0dcf29477d690dac2c4bbc8adc27
Greg <6regmcc@gmail.com>
2023-07-16 15:26:08 +0100

    added more to details page

--- src/routes/detailsPage.jsx
+++ src/routes/detailsPage.jsx
@@ -6,6 +6,7 @@ import img from "../images/film-poster-placeholder.png";
 import Grid from '@mui/material/Unstable_Grid2';
 import Paper from "@mui/material/Paper";
 import {Rating} from "@mui/material";
+import Divider from '@mui/material/Divider';
 
 export default function DetailsPage (props) {
     console.log(props.movie)
@@ -19,8 +20,8 @@ export default function DetailsPage (props) {
                             component="img"
                             sx={{
 
-                                maxHeight:"80%",
-                                maxWidth: "80%"
+                                maxHeight:"90%",
+                                maxWidth: "90%"
                             }}
                             alt="Movie Poster"
                             src={
@@ -32,8 +33,17 @@ export default function DetailsPage (props) {
                     </Grid>
                     <Grid xs={12} sm={6}>
                         <Typography variant="h4" sx={{pb:4}}>{props.movie.title}</Typography>
+                        <Typography variant="body1" sx={{pb:4}}>{props.movie.release_date}</Typography>
                         <Typography align="left">{props.movie.overview}</Typography>
-                        <Rating name="customized-10" precision={0.25} value={props.movie.vote_average} max={10} sx={{textAlign:"left"}} readOnly/>
+                        <Box sx={{display:"inline"}}>
+                            <Rating name="customized-10" precision={0.25} value={props.movie.vote_average} max={10} sx={{textAlign:"left", pt:4}} readOnly/>
+                        </Box>
+
+                        <Typography variant="body2" sx={{display:"inline",m:3}}>{props.movie.vote_count} votes</Typography>
+                        <Divider sx={{py:2}} />
+                        <Typography sx={{py:2}} variant="h6">Genres</Typography>
+
+
                     </Grid>
                 </Grid>
                 </Paper>
