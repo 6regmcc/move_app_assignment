1f6b53da76d0855600270e2712529a635c89d7a4
Greg <6regmcc@gmail.com>
2023-07-16 11:23:32 +0100

    removed text-align: center; from #root

--- src/routes/detailsPage.jsx
+++ src/routes/detailsPage.jsx
@@ -5,6 +5,7 @@ import Box from "@mui/material/Box";
 import img from "../images/film-poster-placeholder.png";
 import Grid from '@mui/material/Unstable_Grid2';
 import Paper from "@mui/material/Paper";
+import {Rating} from "@mui/material";
 
 export default function DetailsPage (props) {
     console.log(props.movie)
@@ -30,8 +31,9 @@ export default function DetailsPage (props) {
                         />
                     </Grid>
                     <Grid xs={12} sm={6}>
-                        <Typography>{props.movie.title}</Typography>
-                        <Typography>{props.movie.overview}</Typography>
+                        <Typography variant="h4" sx={{pb:4}}>{props.movie.title}</Typography>
+                        <Typography align="left">{props.movie.overview}</Typography>
+                        <Rating name="customized-10" precision={0.25} value={props.movie.vote_average} max={10} sx={{textAlign:"left"}} readOnly/>
                     </Grid>
                 </Grid>
                 </Paper>
